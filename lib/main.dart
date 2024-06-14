e9e22bd6c0e30a78461bbfe18ce38234467fe42c
paul.sizon <paul.sizon@outlook.com>
2024-06-13 21:58:58 -0300

    wallet creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'wallet_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider<WalletProvider>(
+    create: (context) => WalletProvider(),
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,11 +15,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final walletProvider = Provider.of<WalletProvider>(context);
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () async {
+                  final mnemonic = walletProvider.generateMnemonic();
+                  final privateKey =
+                      await walletProvider.getPrivateKey(mnemonic);
+                  final publicKey =
+                      await walletProvider.getPublicKey(privateKey);
+                  print("mnemonic: $mnemonic");
+                  print("privateKey: $privateKey");
+                  print("publicKey: $publicKey");
+                },
+                child: Text("Generate Mnemonic")),
+          ],
+        )),
       ),
     );
   }
